565506afeb9f59be12645e6934d8436446539235
Samuel Máximo <samuelgaraujo87@gmail.com>
2024-01-18 20:10:07 -0300

    feat: add data base

---
+++ database/db.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConnectDB() {
+	dsn := "host=172.19.0.2 user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Panic("Connection fail")
+	}
+}
---
+++ migration/docker-database-initial.sql
@@ -0,0 +1,9 @@
+create table personalidades(
+    id serial primary key,
+    nome varchar,
+    historia varchar
+);
+
+INSERT INTO personalidades(nome, historia) VALUES
+('Mão Santa', 'Francisco de Assis de Moraes Souza GOMM (Parnaíba, 13 de outubro de 1942), mais conhecido por Mão Santa, é um médico e político brasileiro filiado ao União Brasil (UNIÃO), atual prefeito de Parnaíba em seu terceiro mandato. Pelo Piauí, foi governador por dois mandatos, senador e deputado estadual. Teve o mandato cassado por corrupção pelo Tribunal Superior Eleitoral (TSE).'),
+('João Claudino', 'Filho de João Claudino Sobrinho e Francisca Fernandes Galiza,[2] o empresário nasceu em Luís Gomes no Rio Grande do Norte e era um dos fundadores do Grupo Claudino ao lado do irmão Valdecy Claudino. O grupo é um dos maiores varejistas do país, com presença em pelo menos 15 estados.');

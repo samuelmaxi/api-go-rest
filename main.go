0e60451492c4be6892ea686ee7ab5b39a9063e9b
Samuel Máximo <samuelgaraujo87@gmail.com>
2024-01-18 20:11:17 -0300

    feat: add models and db

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/samuelmaxi/api-go-rest/models"
+	"github.com/samuelmaxi/api-go-rest/database"
 	"github.com/samuelmaxi/api-go-rest/routes"
 )
 
 func main() {
-	models.Personalities = []models.Personality{
-		{Id: 1, Name: "Samuel", History: "History 123..."},
-		{Id: 2, Name: "Bob", History: "History 321"},
-	}
-
+	database.ConnectDB()
 	fmt.Println("Hello word")
 	routes.HandlerRequest()
 }
--- models/personalities.go
+++ models/personalities.go
@@ -1,9 +1,9 @@
 package models
 
 type Personality struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	History string `json:"history"`
+	Id       int    `json:"id"`
+	Nome     string `json:"nome"`
+	Historia string `json:"historia"`
 }
 
 var Personalities []Personality
